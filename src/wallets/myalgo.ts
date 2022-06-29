52562dc0f90e950d5324c91008671082a5f65da5
Joe Polny <joepolny@gmail.com>
2022-06-29 15:46:18 -0400

    eslint --fix

--- src/wallets/myalgo.ts
+++ src/wallets/myalgo.ts
@@ -1,26 +1,26 @@
-import MyAlgoConnect, { Accounts } from '@randlabs/myalgo-connect';
-import algosdk from 'algosdk';
+import MyAlgoConnect, { Accounts } from '@randlabs/myalgo-connect'
+import algosdk from 'algosdk'
 
 export class MyAlgoSession {
-    myAlgoConnect: MyAlgoConnect
-    accounts: Array<Accounts>
+  myAlgoConnect: MyAlgoConnect
+  accounts: Array<Accounts>
 
-    constructor() {
-        this.myAlgoConnect = new MyAlgoConnect({ disableLedgerNano: false })   
+  constructor () {
+    this.myAlgoConnect = new MyAlgoConnect({ disableLedgerNano: false })
+  }
+
+  async getAccounts () {
+    const settings = {
+      shouldSelectOneAccount: false,
+      openManager: true
     }
 
-    async getAccounts(){
-        const settings = {
-            shouldSelectOneAccount: false,
-            openManager: true
-        };
-        
-        this.accounts = await this.myAlgoConnect.connect(settings)
+    this.accounts = await this.myAlgoConnect.connect(settings)
 
-        return this.accounts
-    }
+    return this.accounts
+  }
 
-    async signTxns(unsignedTxns: Array<algosdk.Transaction>){
-        this.myAlgoConnect.signTransaction(unsignedTxns.map(txn => txn.toByte()))
-    }
+  async signTxns (unsignedTxns: Array<algosdk.Transaction>) {
+    this.myAlgoConnect.signTransaction(unsignedTxns.map(txn => txn.toByte()))
+  }
 }
