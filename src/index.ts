1d382a9df867613384c5ecb60ac4b79c9930b0c2
Joe Polny <joepolny@gmail.com>
2022-06-29 17:11:27 -0400

    add walletconnect

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import algosdk from 'algosdk'
 // import { MyAlgoSession } from './wallets/myalgo'
+// import { WalletConnectSession } from './wallets/walletconnect'
 
 console.log(`Generated Algorand account: ${algosdk.generateAccount().addr}`)
