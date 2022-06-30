d3cbe33bb26e7436b1198d58a4aee8667090a703
Joe Polny <joepolny@gmail.com>
2022-06-30 09:21:53 -0400

    add AlgoSigner

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import algosdk from 'algosdk'
 // import { MyAlgoSession } from './wallets/myalgo'
 // import { WalletConnectSession } from './wallets/walletconnect'
+// import { AlgoSignerSession } from './wallets/algosigner'
 
 console.log(`Generated Algorand account: ${algosdk.generateAccount().addr}`)
